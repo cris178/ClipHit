cbd7a58ceccb8b3eccae7b96cf84edfe08bd27a1
Cristian <cris178@outlook.com>
2020-11-03 21:42:29 -0800

    added swiper

--- README.md
+++ README.md
@@ -2,8 +2,19 @@
 A third party Twitch App.
 
 
+# Place Holder Images
+
+https://placeholder.com/
+
 # React-Router-DOM
 
 https://seegatesite.com/tutorial-on-react-router-dom-for-beginners/
 
 Can use routing for web and mobile apps
+
+
+# React Swiper
+
+https://www.npmjs.com/package/react-id-swiper
+
+https://react-id-swiper.ashernguyen.site/example/default
--- src/components/mobileview/MobileView.js
+++ src/components/mobileview/MobileView.js
@@ -1,12 +1,46 @@
 import React,{useEffect,useState} from 'react';
 import "./MobileView.css"
+import 'swiper/swiper-bundle.css';
+import Swiper from 'react-id-swiper';
+import { Pagination, Navigation, Autoplay } from "swiper";
 
 
-function MobileView(){
-    console.log("Rendering Mobile")
+function MobileView(props){
+    console.log("Rendering Mobile");
+    const [parallaxSwiper, setParallaxSwiper] = useState(null);
+    const [swiper, updateSwiper] = useState(null);
+
+    const params = {
+        modules: [Pagination, Navigation, Autoplay],
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets",
+          clickable: true
+        },
+        autoplay: {
+          delay: 1000,
+          disableOnInteraction: false
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        },
+        spaceBetween: 30,
+        getSwiper: updateSwiper
+      };
+
     return(
         <div className="MobileView">
-            Hello Mobile!
+            
+
+           <Swiper {...params} >
+                <div><img src="https://via.placeholder.com/350x150"/></div>
+                <div><img src="https://via.placeholder.com/350x150"/></div>
+                <div><img src="https://via.placeholder.com/350x150"/></div>
+                <div><img src="https://via.placeholder.com/350x150"/></div>
+                <div><img src="https://via.placeholder.com/350x150"/></div>
+            </Swiper>
         </div>
     );
 }
