e01772d71eff68e1bb3898bc607a8337898fc49b
Cristian <cris178@outlook.com>
2020-11-03 19:20:25 -0800

    desktop view mockups set

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ClipHit
 A third party Twitch App.
+
+
+# React-Router-DOM
+
+https://seegatesite.com/tutorial-on-react-router-dom-for-beginners/
+
+Can use routing for web and mobile apps
--- src/components/desktopview/DesktopView.js
+++ src/components/desktopview/DesktopView.js
@@ -9,18 +9,36 @@ function DesktopView(){
         <div className="DesktopView">
            <div className="hero">
                 <nav><h1 className="title">HitPlay </h1> <div className="download">Switch to Mobile</div></nav>
-                <ul>
-                    <li><h1>mobile</h1></li>
-                    <li><h1>streaming</h1></li>
-                    <li><h1>video on demand</h1></li>
-                </ul>
+                <div className="advert">
+                    <ul className="advertMessage">
+                        <div className="flourish"></div>
+                        <li><h1>mobile</h1></li>
+                        <li><h1>streaming</h1></li>
+                        <li><h1>video on demand</h1></li>
+                    </ul>
+                    <div className="advertPhoto">
+                        <img src="https://via.placeholder.com/350x150" />
+                    </div>
+                </div>
+                
            </div>
            <div className="showCase">
                 <div className="otherApps">
                     <h3 className="callToAction">Other apps</h3>
                 </div>
                 <div className="flow">
-                   <div className="logos">Apps</div>
+                   <div className="logos">
+                    <img className="logoImage" src="https://via.placeholder.com/140x100"></img>
+                    <p>app name</p>
+                    </div>
+                   <div className="logos">
+                    <img className="logoImage" src="https://via.placeholder.com/140x100"></img>
+                    <p>app name</p>
+                    </div>
+                   <div className="logos">
+                    <img className="logoImage" src="https://via.placeholder.com/140x100"></img>
+                    <p>app name</p>
+                    </div>
                 </div>
            </div>
         </div>
